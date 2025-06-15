3436cbd2d42d9915d7b35444950305c6ffe37e6d
Steven <156148848+estibenjack@users.noreply.github.com>
2025-06-15 22:53:37 +0100

    Fix css for header and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Dashboard from "./pages/Dashboard";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Dashboard />
+      {/* <Footer /> */}
     </>
   );
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import MainContent from "../components/MainContent";
 import AddKafBtn from "../components/AddKafBtn";
 import AddKafModal from "../components/AddKafModal";
 import classes from "./css/Dashboard.module.css";
+import Footer from "../components/Footer";
 
 export default function Dashboard() {
   const [kafList, setKafList] = useState(kafs);
@@ -36,6 +37,7 @@ export default function Dashboard() {
           />
         )}
       </div>
+      <Footer />
     </div>
   );
 }
