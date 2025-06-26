331f464824058ecd9d19bb7fd7f785cbd9a6163c
Steven <156148848+estibenjack@users.noreply.github.com>
2025-06-27 00:49:30 +0100

    Add new endpoints/handler methods + comments

--- backend/src/main/java/com/estibenjack/kaftrack/controller/KafController.java
+++ backend/src/main/java/com/estibenjack/kaftrack/controller/KafController.java
@@ -7,23 +7,46 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// this class handles HTTP requests for kaf resources
+// @RestController means it returns data directly (JSON usually)
+// @RquestMapping("/kafs") means all URLS here will start with /kafs
 @RestController
 @RequestMapping("/kafs")
 public class KafController {
     private final KafService kafService;
 
+    // constructor based dependency injection
+    // spring calls this and provides a KafService instance automatically
+    // this avoids tightly coupling (by KafController NOT creating the KafService itself)
     public KafController(KafService kafService) {
         this.kafService = kafService;
     }
 
+    // this is to GET all kafs
     @GetMapping
     public List<Kaf> getAllKafs(){
         return kafService.getAllKafs();
     }
 
+    // this is to GET a single kaf by its id - handles GET requests to /kafs/{id}
+    // {id} is a path variable that gets passed into the parameter as id
+    @GetMapping("/{id}")
+    public ResponseEntity<Kaf> getKafById(@PathVariable String id){
+        Kaf kaf = kafService.getKafById(id);
+        if (kaf!=null){
+            return ResponseEntity.ok(kaf);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // creates a new kaf entry - handles POST requests to /kafs
+    // no extra path here because POST for new entries should be done to /kafs
+    // @RequestBody in the parameters tells spring to convert JSON from the request into a KAF obj
     @PostMapping
     public ResponseEntity<Kaf> createKaf(@RequestBody Kaf kaf) {
         Kaf savedKaf = kafService.createKaf(kaf);
         return ResponseEntity.ok(savedKaf);
     }
+
 }
